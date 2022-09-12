bb6f9a2c5f81362dacb062d57015e29a5a236aea
prabhat025dec <105845220+prabhat025dec@users.noreply.github.com>
2022-09-12 23:25:46 +0530

    Update app.js
    password hide

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 const mongoose=require("mongoose");
 
-mongoose.connect('mongodb+srv://prabhat-admin:MPrabhat%401234@cluster0.zi8uqcw.mongodb.net/todolistDB',{useNewUrlParser:true},{useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://*************@cluster0.zi8uqcw.mongodb.net/todolistDB',{useNewUrlParser:true},{useUnifiedTopology: true});
 
 app.set('view engine', 'ejs');
 
